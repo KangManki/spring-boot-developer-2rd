502c7cc420501a6f06bd4b36d319e3663ccd17e7
mkkang <mk.kang@dongbuled-s.com>
2024-09-10 17:20:24 +0900

    start : 프로젝트 첫 커밋

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,25 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.User;
+import me.shinsunyoung.springbootdeveloper.dto.AddUserRequest;
+import me.shinsunyoung.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+
+
+}
